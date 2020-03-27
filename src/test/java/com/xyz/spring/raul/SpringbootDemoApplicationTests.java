42e9b3cff1082bcddfe0b1b7d731666b32048d82
Raul <e.rahuljacob@gmail.com>
2020-03-27 16:55:56 +0530

    Initial Springboot Setup

---
+++ src/test/java/com/xyz/spring/raul/SpringbootDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.xyz.spring.raul;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
