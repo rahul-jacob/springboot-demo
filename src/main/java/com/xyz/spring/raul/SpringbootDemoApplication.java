017338b4250b04ed21777e110bbdc5aeb23aaf71
Raul <e.rahuljacob@gmail.com>
2020-04-05 09:54:36 +0530

    Context obtained

--- src/main/java/com/xyz/spring/raul/SpringbootDemoApplication.java
+++ src/main/java/com/xyz/spring/raul/SpringbootDemoApplication.java
@@ -2,12 +2,20 @@ package com.xyz.spring.raul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.xyz.spring.raul.controllers.MyController;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringbootDemoApplication.class, args);
+		//By default every bean will be created in the context will have the name of the class name
+		//with the first letter in lower case so MyController in the context will be myController
+		MyController mycontroller = (MyController) ctx.getBean("myController");
+		String greeting = mycontroller.sayHello();
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/main/java/com/xyz/spring/raul/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.xyz.spring.raul.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+	
+	public String sayHello() {
+		return "Hai Folks";
+	}
+
+}
