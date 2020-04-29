5f12bc9dbdc1a5dea62b88b70f3137d8e572fc56
Raul <e.rahuljacob@gmail.com>
2020-04-29 22:34:25 +0530

    Added BaseEntity for model objects

--- pet-clinic-data/src/main/java/com/xyz/spring/raul/model/Pet.java
+++ pet-clinic-data/src/main/java/com/xyz/spring/raul/model/Pet.java
@@ -2,7 +2,7 @@ package com.xyz.spring.raul.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
 	
 	private PetType petType;
 	private Owner owner;
--- pet-clinic-data/src/main/java/com/xyz/spring/raul/model/PetType.java
+++ pet-clinic-data/src/main/java/com/xyz/spring/raul/model/PetType.java
@@ -1,6 +1,6 @@
 package com.xyz.spring.raul.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
 	private String name;
 
