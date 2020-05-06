baa6a975ee11bc052810291e452701782f95377b
Raul <e.rahuljacob@gmail.com>
2020-05-06 10:48:12 +0530

    modified source

--- src/test/java/com/xyz/spring/raul/SpringbootDemoApplicationTests.java
+++ src/test/java/com/xyz/spring/raul/SpringbootDemoApplicationTests.java
@@ -1,14 +1,16 @@
 package com.xyz.spring.raul;
 
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootDemoApplicationTests {
+public class SpringbootDemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
